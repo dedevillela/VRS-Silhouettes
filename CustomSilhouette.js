d89899a4f1d38bd9b06df6f0d905881ae54621a1
André Villela <dede.villela@gmail.com>
2017-03-14 16:44:49 -0300

    Added script file

---
+++ CustomSilhouette.js
@@ -0,0 +1,7 @@
+﻿<script type="text/javascript" async>
+    if(VRS && VRS.globalDispatch && VRS.serverConfig) {
+        VRS.globalDispatch.hook(VRS.globalEvent.bootstrapCreated, function(bootStrap) {
+        	VRS.globalOptions.aircraftAllowRegistrationFlagOverride = true;
+        });
+    }
+</script>
