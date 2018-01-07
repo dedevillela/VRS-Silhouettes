f2c8c93441b23e76a9849a9dfb3c63a255052ef7
André Villela <dede.villela@gmail.com>
2018-01-07 13:20:59 -0200

    Update CustomSilhouette.js

--- CustomSilhouette.js
+++ CustomSilhouette.js
@@ -1,7 +1,6 @@
-﻿<script type="text/javascript" async>
-    if(VRS && VRS.globalDispatch && VRS.serverConfig) {
-        VRS.globalDispatch.hook(VRS.globalEvent.bootstrapCreated, function(bootStrap) {
-        	VRS.globalOptions.aircraftAllowRegistrationFlagOverride = true;
-        });
-    }
-</script>
+/** global: VRS */
+if(VRS && VRS.globalDispatch && VRS.serverConfig) {
+	VRS.globalDispatch.hook(VRS.globalEvent.bootstrapCreated, function(bootStrap) {
+		VRS.globalOptions.aircraftAllowRegistrationFlagOverride = true;
+	});
+}
